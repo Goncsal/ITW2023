9b42bad94eb758cfb093bee5e37917da780c8655
dani1244 <121317384+dani1244@users.noreply.github.com>
2023-12-04 14:13:12 +0000

    hh

--- playerDetail.js
+++ playerDetail.js
@@ -3,7 +3,7 @@ var vm = function () {
     console.log('ViewModel initiated...');
     //---Variáveis locais
     var self = this;
-    self.baseUri = ko.observable('http://192.168.160.58/NBA/API/Player/');
+    self.baseUri = ko.observable('http://192.168.160.58/NBA/API/Players/');
     self.displayName = 'NBA Players Details';
     self.error = ko.observable('');
     self.passingMessage = ko.observable('');
